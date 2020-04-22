a82190e80e954ad8c127dd4205380ea5de218632
rad3enko <rad3enko@gmail.com>
2020-04-22 17:41:04 +0300

    add screenshot

---
+++ src/main/java/ru/booking/action/argument/BookActionArgument.java
@@ -0,0 +1,15 @@
+package ru.booking.action.argument;
+
+import ru.booking.util.action.ActionArgument;
+
+/**
+ * Created on 05.03.2020.
+ *
+ * @author Sergey Radchenko
+ */
+public class BookActionArgument implements ActionArgument {
+    @Override
+    public boolean isValid() {
+        return false;
+    }
+}
